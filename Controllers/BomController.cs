df11880dd7000a7f50200806e96023bf29804fac
student01 gfpt <student01-gfpt@gfpt.co.th>
2022-10-07 11:54:44 +0700

    popup

---
+++ Controllers/BomController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentHtml.Models;
+
+namespace StudentHtml.Controllers
+{
+    public class BomController : Controller
+    {
+        private readonly ILogger<BomController> _logger;
+
+        public BomController(ILogger<BomController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
